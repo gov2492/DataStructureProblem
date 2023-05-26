a7f7c249d9ec5afeccb90a2a8525c262bcd14ad5
gov2492 <35134455+gov2492@users.noreply.github.com>
2023-05-26 19:34:58 +0530

    Update prime.java

--- src/MatheMatics/prime.java
+++ src/MatheMatics/prime.java
@@ -1,11 +1,13 @@
    static boolean isPrime(int n){
-
+    //Checking at 1
         if(n==1){
             return false;
         }
+      //Checking all 2 and 3 divisor (2,4,3,6,8,9,10,12,14,15,16)
         if(n%2==0||n%3==0){
             return false;
         }
+       //Checking all 2 and 3 divisor (5,7,11,13.....)
         for(int i=5;i*i<=n;i=i+6){
             if(n%i==0 ||n%(i+2)==0){
                 return false;
@@ -14,3 +16,5 @@
         return true;
         
     }
+
+//read me
